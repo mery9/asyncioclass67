a71dbba12af1dc2dc378a70bcc0a41991d7619e4
Kantinan Boonyingsathit <kaseam008@gmail.com>
2024-09-07 00:18:29 +0700

    Comment Assignment11

--- assignment11/taskgroup03.py
+++ assignment11/taskgroup03.py
@@ -1,17 +1,33 @@
 import time
 import asyncio
 from asyncio import Queue, TaskGroup
+from random import randrange
 
+# we first implement the Customer and Product classes, 
+# representing customers and products that need to be checked out. 
+# The Product class has a checkout_time attribute, 
+# which represents the time required for checking out the product.
 class Product:
-    def __init__(self, product_name: str, base_checkout_time: float):
+    def __init__(self, product_name: str, checkout_time: float):
         self.product_name = product_name
-        self.base_checkout_time = base_checkout_time
+        self.checkout_time = checkout_time
 
 class Customer:
     def __init__(self, customer_id: int, products: list[Product]):
         self.customer_id = customer_id
         self.products = products
 
+# we implement a checkout_customer method that acts as a consumer.
+# As long as there is data in the queue, this method will continue to loop. 
+# During each iteration, it uses a get method to retrieve a Customer instance. 
+# 
+# If there is no data in the queue, it will wait. 
+# 
+# After retrieving a piece of data (in this case, a Customer instance), 
+# it iterates through the products attribute and uses asyncio.sleep to simulate the checkout process.
+# 
+# After finishing processing the data, 
+# we use queue.task_done() to tell the queue that the data has been successfully processed.
 async def checkout_customer(queue: Queue, cashier_number: int):
     cashier_total_time = 0  # Initialize cashier's total time
     cashier_start_time = time.perf_counter()  # Start time for the cashier
@@ -28,7 +44,7 @@ async def checkout_customer(queue: Queue, cashier_number: int):
                 adjusted_checkout_time = 0.1
             else:
                 # Adjust checkout time based on cashier number using formula 1 + (0.1 * cashier_number)
-                adjusted_checkout_time = product.base_checkout_time + (0.1 * cashier_number)
+                adjusted_checkout_time = product.checkout_time + (0.1 * cashier_number)
             
             print(f"The Cashier_{cashier_number} "
                   f"will checkout Customer_{customer.customer_id}'s "
@@ -48,23 +64,30 @@ async def checkout_customer(queue: Queue, cashier_number: int):
     cashier_total_time = time.perf_counter() - cashier_start_time
     return cashier_number, customer_count, round(cashier_total_time, ndigits=2)
 
+
+# we implement the generate_customer method as a factory method for producing customers.
+#
+# We first define a product series and the required checkout time for each product. 
+# Then, we place 0 to 4 products in each customer’s shopping cart.
 def generate_customer(customer_id: int) -> Customer:
-    # Base checkout times for products
     all_products = [Product('beef', 1),
                     Product('banana', .4),
                     Product('sausage', .4),
                     Product('diapers', .2)]
     return Customer(customer_id, all_products)
 
+# we implement the customer_generation method as a producer. 
+# This method generates several customer instances regularly 
+# and puts them in the queue. If the queue is full, the put method will wait.
 async def customer_generation(queue: Queue, customers: int):
     customer_count = 0
     while True:
         customers = [generate_customer(the_id)
                      for the_id in range(customer_count, customer_count + customers)]
         for customer in customers:
-            print("waiting to put customer in line...")
+            print(f"Waiting to put Customer_{customer.customer_id} in line.... ")
             await queue.put(customer)
-            print("Customer put in line...")
+            print(f"Customer_{customer.customer_id} put in line...")
         customer_count = customer_count + len(customers)
         await asyncio.sleep(.001)
         return customer_count
